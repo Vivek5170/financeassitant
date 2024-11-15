67a0718be30f5014d61add2312df0c855df30d6b
Vivek5170 <75850017+Vivek5170@users.noreply.github.com>
2024-11-15 18:29:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -281,4 +281,4 @@ def handle_prompt():
 # Run the Flask app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=false)
+    app.run(host="0.0.0.0", port=port, debug=False)
